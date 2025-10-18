12e81a0d0d25e2b813a262bbfbd36a20dfb9f677
unknown <aseniyathejan224@gmail.com>
2025-10-18 13:19:36 +0530

    completed FAQ al

---
+++ app/controllers/Alumni/Faq.php
@@ -0,0 +1,16 @@
+<?php
+class Faq extends Controller
+{
+    public function index()
+    {
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (empty($_SESSION['user_id']) || $_SESSION['role'] !== 'alumni') {
+            redirect('alumni/auth');
+        }
+
+        $this->view('alumni/faq');
+    }
+}
